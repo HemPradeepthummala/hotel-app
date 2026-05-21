9bb32048916518dc541f5438131742a256b54923
HemPradeepthummala <pradeeproyalthummala@Thummalas-MacBook-Pro.local>
2026-05-21 11:26:36 +0530

    creates the hotels and hotel record

---
+++ src/main/java/org/example/hotelapp/models/Hotel.java
@@ -0,0 +1,4 @@
+package org.example.hotelapp.models;
+
+public record Hotel(String id, String name, int rooms, String city) {
+}
---
+++ src/main/java/org/example/hotelapp/models/Hotels.java
@@ -0,0 +1,11 @@
+package org.example.hotelapp.models;
+
+import java.util.ArrayList;
+
+public class Hotels {
+	private final ArrayList<Hotel> hotels;
+
+	public Hotels(ArrayList<Hotel> hotels) {
+		this.hotels = hotels;
+	}
+}
